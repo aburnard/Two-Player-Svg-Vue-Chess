04c2c8c96d72954f249655b14eb1b705eab1365b
aburnard <36141001+aburnard@users.noreply.github.com>
2021-01-03 12:41:48 +1100

    Initial Commit

---
+++ chessgarden/src/components/BoardSquare.vue
@@ -0,0 +1,55 @@
+<template>
+  <g>
+    <circle
+      @click="alert"
+      cx="300"
+      cy="1000"
+      r="40"
+      stroke="green"
+      stroke-width="4"
+      fill="red"
+    />
+    <rect
+      @click="alert"
+      :x="computedXCoord"
+      :y="computedYCoord"
+      :width="sideLength"
+      :height="sideLength"
+      stroke="green"
+      stroke-width="8"
+      fill="red"
+    />
+  </g>
+</template>
+
+<script>
+export default {
+  methods: {
+    alert() {
+      this.$buefy.dialog.alert(
+        "x is " + this.xCoord + "ycor is " + this.computedYCoord
+      );
+    },
+  },
+  props: ["xCoord"],
+  data() {
+    return {
+      sideLength: 100,
+    };
+  },
+  computed: {
+    computedCoord() {
+      return this.xCoord * 3;
+    },
+    computedXCoord() {
+      return (
+        this.xCoord * this.sideLength -
+        Math.floor(this.xCoord / 8) * this.sideLength * 8
+      );
+    },
+    computedYCoord() {
+      return Math.floor(this.xCoord / 8) * this.sideLength;
+    },
+  },
+};
+</script>
---
+++ chessgarden/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <svg width="20000" height="20000">
+      <circle
+        cx="400"
+        cy="200"
+        r="100"
+        stroke="green"
+        stroke-width="4"
+        fill="yellow"
+      />
+      <g v-for="(item, index) in noSquares" :key="index">
+        <BoardSquare :id="key" :xCoord="index" :key="index * key" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+import BoardSquare from "@/components/BoardSquare.vue";
+export default {
+  name: "HelloWorld",
+  components: {
+    BoardSquare,
+  },
+  data() {
+    return {
+      noSquares: 64,
+    };
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
